e848cc610f2fa787f1bc020b3691c111516d4db6
Doreen <doreenlulu97@gmail.com>
2022-11-09 21:26:23 +0300

    solved

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,9 @@
 class Game < ActiveRecord::Base
   has_many :reviews
+  has_many :users, through: :reviews
 end
+
+# class Game < ActiveRecord::Base
+#   has_many(:reviews)
+#   has_many(:users, through: :reviews)
+# end
